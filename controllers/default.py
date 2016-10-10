d2b96b1c57d900eb7e440c2c6d43e418bbd8b804
Luca de Alfaro <luca@dealfaro.org>
2016-10-09 17:27:55 -0700

    Added function to return a user name

--- controllers/default.py
+++ controllers/default.py
@@ -8,6 +8,15 @@
 # - download is for downloading files uploaded in the db (does streaming)
 # -------------------------------------------------------------------------
 
+def get_user_name_from_email(email):
+    """Returns a string corresponding to the user first and last names,
+    given the user email."""
+    u = db(db.auth_user.email == email).select().first()
+    if u is None:
+        return 'None'
+    else:
+        return ' '.join([u.first_name, u.last_name])
+
 
 def index():
     """
